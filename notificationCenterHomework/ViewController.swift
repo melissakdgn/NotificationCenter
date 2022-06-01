6ea4d26e3708ff1dd9f57c4839c915ed04b81c13
Melis Akdoğan <77884894+melissakdgn@users.noreply.github.com>
2022-06-01 20:43:11 +0300

    Update ViewController.swift

--- notificationCenterHomework/ViewController.swift
+++ notificationCenterHomework/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
-
-
+    //ViewController ve SecondViewController background rengini değiştirmek üzere notification gönderen fonksiyon
+    @IBAction func buttonFunction(_ sender: Any) {
+        NotificationCenter.default.post(name: Notification.Name("colorChange"),object: nil)
+        view.backgroundColor = .yellow
+    } //Gönderdiğimiz notification'a bir isim atıyoruz. Bu view içerisinde gerçekleşecek actionları parantez içerisinde belirtiyoruz. Ayrıca obje gönderilmediği için nil olarak tanımlanıyor 
+    
+    // SecondViewController label text rengini değiştirmek üzere notification gönderen fonksiyon
+    @IBAction func titleFunction(_ sender: Any) {
+        NotificationCenter.default.post(name:Notification.Name("titleColor"),object: nil)
+        
+    }
+    
 }
-
