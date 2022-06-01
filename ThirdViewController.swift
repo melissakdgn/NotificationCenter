49bf44127758911dd227381e3c7571e1aa72f667
Melis Akdoğan <77884894+melissakdgn@users.noreply.github.com>
2022-06-01 20:41:51 +0300

    Update ThirdViewController.swift

--- ThirdViewController.swift
+++ ThirdViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ThirdViewController.swift
+//  SecondViewController.swift
 //  notificationCenterHomework
 //
 //  Created by Melis Akdoğan on 1.06.2022.
@@ -7,21 +7,28 @@
 
 import UIKit
 
-class econdViewController: UIViewController {
+class SecondViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        NotificationCenter.default.addObserver(self, selector: #selector(isNotificationReceived), name: Notification.Name("colorChanged"), object: nil)
         
+        //addObserver metodu ile gelen notification'a dinleyici ekliyoruz.Tetiklenecek olan fonksiyonu select parametresi ile belirtiyoruz
+        NotificationCenter.default.addObserver(self, selector: #selector(isNotificationReceived), name: Notification.Name("colorChange"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(isSecondNotificationReceived), name: Notification.Name("titleColor"), object: nil)
     }
     
+    //Notification iletildiğinde tetiklenen fonksiyonların declarationları
     @objc func isNotificationReceived(){
         
         view.backgroundColor = .black
         
     }
-
     
-
+    @objc func isSecondNotificationReceived(){
+        label.textColor = .yellow
+    }
+   
 }
